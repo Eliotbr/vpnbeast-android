5abb3dc7b004144aaa53df931efb6c63b195c5e1
gitlab <bilalccaliskan@gmail.com>
2018-03-27 22:29:14 +0300

    gitlab to github mirror test

--- app/src/main/java/com/b/android/openvpn60/model/Mail.java
+++ app/src/main/java/com/b/android/openvpn60/model/Mail.java
@@ -62,6 +62,7 @@ public class Mail extends javax.mail.Authenticator {
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.socketFactory.fallback", "false");
         props.setProperty("mail.smtp.quitwait", "false");
+
     }
 
     public boolean sendEmail() {
