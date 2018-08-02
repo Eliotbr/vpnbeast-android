05e1f101e814b4c6b9a8e23bdfa925f26143cc37
= <bilalccaliskan@gmail.com>
2018-08-02 19:57:31 +0300

    vpnbeast-service endpoint change

--- app/src/main/java/com/b/android/openvpn60/constant/ServiceConstants.java
+++ app/src/main/java/com/b/android/openvpn60/constant/ServiceConstants.java
@@ -5,12 +5,12 @@ package com.b.android.openvpn60.constant;
  */
 
 public enum ServiceConstants {
-    URL_INSERT_USER("http://139.59.160.203:8080/rest/user-controller/insert-user"),
-    URL_INSERT_MEMBER("http://139.59.160.203:8080/rest/member-controller/insert-member"),
-    URL_LOGIN("http://139.59.160.203:8080/rest/user-controller/do-login"),
-    URL_UPDATE_LAST_LOGIN("http://139.59.160.203:8080/rest/user-controller/update-last-login"),
-    URL_GET_PROFILES("http://139.59.160.203:8080/rest/server-controller/get-all-servers"),
-    URL_GET_MEMBER_BY_USERNAME("http://139.59.160.203:8080/rest/member-controller/members");
+    URL_INSERT_USER("http://139.59.160.203:8080/vpnbeast-service/user-controller/insert-user"),
+    URL_INSERT_MEMBER("http://139.59.160.203:8080/vpnbeast-service/member-controller/insert-member"),
+    URL_LOGIN("http://139.59.160.203:8080/vpnbeast-service/user-controller/do-login"),
+    URL_UPDATE_LAST_LOGIN("http://139.59.160.203:8080/vpnbeast-service/user-controller/update-last-login"),
+    URL_GET_PROFILES("http://139.59.160.203:8080/vpnbeast-service/server-controller/get-all-servers"),
+    URL_GET_MEMBER_BY_USERNAME("http://139.59.160.203:8080/vpnbeast-service/member-controller/members");
 
 
     private final String text;
